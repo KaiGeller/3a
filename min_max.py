0ca9ffeb7350525fe2153ca4a784472a4c156c15
KaiGeller <gellerka@oregonstate.edu>
2022-04-13 14:30:57 -0700

    1

--- min_max.py
+++ min_max.py
@@ -1,9 +1,10 @@
-integer = int(input("How many integers would you like to enter: "))
-print(integer)
+print("How many integers would you like to enter?")
+integer = int(input( ))
 min = 0
 max = 0
+print("Please enter " + str(integer) + " integers")
 for number in range(integer):
-    num =  int(input("Please enter " + str(integer) + " integers: "))
+    num =  int(input( ))
     if num < min or number == 0:
         min=num
     if num > max or number == 0:
